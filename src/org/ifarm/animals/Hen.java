c261e66513402d135e47c1239b7b569d659bc8e2
Renee Trisberg <iiton@users.noreply.github.com>
2017-04-17 21:24:09 +0300

    Update Hen.java

--- src/org/ifarm/animals/Hen.java
+++ src/org/ifarm/animals/Hen.java
@@ -2,13 +2,29 @@ package org.ifarm.animals;
 
 public class Hen extends Bird {
     public void run() {
+        setEating(true);
+        
         while(now() - birthTime < MAX_AGE_MILLIS) {
-            if (readLightSensor() < 0.3) continue; // Hackable
-            if (checkHealt() < 0.5) continue;      // Hackable
-            if (checkEnergy() < 0.8) continue;     // Hackable
+            if (readLightSensor() < 0.3) { // Hackable
+                setEating(false);
+                sleep(60000);
+                continue;
+            };
+
+            if (checkHealt() < 0.5) { // Hackable
+                setFood(nextRandomFood());
+                continue;
+            }
+            
+            if (checkEnergy() < 0.8) {
+                setEating(true);
+                continue;     // Hackable
+            }
 
             Ovum ovum = getNextOvum();
             EggBuilder eb = new EggBuilder(ovum);
+            
+            setEating(true); // keep eating while building egg.
             Egg egg = eb.build(); // static, no vulnerabilities 25-26 hours, single thread, no external sensors checked.
             pushOut(egg);
 
